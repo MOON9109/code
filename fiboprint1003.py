db542ceb5b39b371ac1dc0d070789b28e6ee56df
moon9109 <hanhannetnet@naver.com>
2020-12-23 17:30:52 +0900

    Add Python File [fiboprint1003.py]

---
+++ fiboprint1003.py
@@ -0,0 +1,11 @@
+import sys
+T = int(input())
+dp = [[1,0], [0,1]]
+q = [int(sys.stdin.readline()) for _ in range(T)]
+
+for i in range(2,max(q)+1):
+    dp.append([dp[i-2][0]+dp[i-1][0], dp[i-2][1]+dp[i-1][1]])
+print(dp)
+print(q)
+for i in q:
+    print(dp[i][0], dp[i][1])
