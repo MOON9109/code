0a00104f7923f458aa6ac3a9a42d03b3b7379cd0
moon9109 <hanhannetnet@naver.com>
2021-02-10 18:39:03 +0900

    Add Python file[음계.py]

---
+++ 음계.py
@@ -0,0 +1,22 @@
+# 음계
+
+a=list(map(int, input().split(' ')))
+
+ascending =True
+
+descending = True
+
+
+for i in range(1, 8):
+    if a[i]> a[i -1]:
+       descending =False
+
+    if a[i] <a[i-1]:
+       ascending = False
+
+if ascending:
+    print('ascending')
+elif descending:
+    print('descending')
+else:
+    print('mixed')
