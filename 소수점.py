278e97087411b49d0f128c68cbf7b38698bcde0a
moon9109 <hanhannetnet@naver.com>
2021-03-14 22:14:59 +0900

    Add python file [소수점.py]

---
+++ 소수점.py
@@ -0,0 +1,10 @@
+a=5.12345
+b=10.12345
+print('a값: %.5f'%a)
+print('a값: {}'.format(a,'.5f'))
+print('a값: {0:.5f}'.format(a))
+
+print('a, b값: %.5f %.5f'%(a,b))
+
+print('a값 b값 : {0:.5f} {1:.5f}'.format(a,b))
+
