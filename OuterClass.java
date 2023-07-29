33990f5dbc4d7f48256bb11c93c3b1f9e385b25f
moon91 <hanhannetnet@naver.com>
2023-07-29 22:15:33 +0900

    Add java file [OuterClass.java]

---
+++ OuterClass.java
@@ -0,0 +1,19 @@
+package fastcampus.de.clip5;
+
+public class OuterClass {
+
+    public static void main(String[] args){
+        System.out.println("outer class");
+        InnerClass innerClass = new InnerClass();
+        innerClass.printName();
+
+    }
+    public static class InnerClass {
+        public void printName(){
+
+            System.out.println(getClass().getName());
+            System.out.println(this.getClass().getSimpleName());
+        }
+
+    }
+}
