a5d9d2faa741dbcc647a8aeb271e11a42b0f88f0
moon91 <hanhannetnet@naver.com>
2023-07-30 00:15:41 +0900

    Add java file [Numbers.java]

---
+++ Numbers.java
@@ -0,0 +1,31 @@
+package fastcampus.de.clip7;
+
+public class Numbers {
+    public static void main(String[] args) {
+
+
+        short s =1;
+        System.out.println(s);
+
+        long b =123457L;
+        System.out.println(b);
+
+        float c =5.5F;
+        System.out.println(c);
+
+        double d =9.12d;
+        System.out.println(d);
+
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Short.MIN_VALUE);
+
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE);
+
+        System.out.println(Float.MAX_VALUE);
+        System.out.println(Float.MIN_VALUE);
+
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Double.MIN_VALUE);
+    }
+}
