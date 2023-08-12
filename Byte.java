d2c94ff1557224eb19efbed4700af718225e9081
moon91 <hanhannetnet@naver.com>
2023-08-12 23:35:42 +0900

    Add java file

---
+++ Byte.java
@@ -0,0 +1,14 @@
+package fastcampus.de.clip7;
+import java.util.Arrays;
+public class Byte {
+    public static void main(String[] args) {
+
+        String str = "hello world" ;
+        System.out.println(str);
+
+        int [] intArray = new int[] {1,2,3,4,5};
+        System.out.println(Arrays.toString(intArray));
+
+
+    }
+    }
