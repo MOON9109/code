fd96f6681ccfb64046064e164144e364adfcb0a4
moon91 <hanhannetnet@naver.com>
2025-02-07 21:31:37 +0900

    Add HSAT 5회 code

--- 업무처리(HSAT 5).py
+++ 업무처리(HSAT 5).py
@@ -1,22 +1,32 @@
-
 H, K, R = map(int, input().split())
-works = [list(map(int, input().split())) for _ in range( 2* *H)] # 처음 말단 직원의 업무
-staffCount = 2** (H + 1) - 1  # 전체 직원의 수
-organization = [[] for _ in range(staffCount + 1)]  # 1부터 처리하기 위해 +1
-for i in range(len(works)):
-    organization[staffCount - i] = works[-i - 1]  # 말단 직원의 번호에 업무 할당
-notEndStaff = 2 ** (H)  # 말단 직원이 아닌 직원의 번호
-complete = 0
-for day in range(R):
-    for t in range(1, notEndStaff):
-        if t == 1:
-            if len(organization[1]) != 0:  # 부서장의 업무 중 처리된 업무가 있다면 그 번호를 더함
-                complete += organization[1].pop(0)
-        if day % 2 == 1:  # 날짜가 홀수일 때
-            if len(organization[t * 2]) != 0:
-                organization[t].append(organization[t * 2].pop(0))
-        elif day % 2 == 0:  # 날짜가 짝수일 때
-            if len(organization[t * 2 + 1]) != 0:
-                organization[t].append(organization[t * 2 + 1].pop(0))
+work = [list(map(int, input().split())) for _ in range(2 ** H)]
+people = 2 ** (H + 1) - 1  # 직원 숫자
+company = [[] for _ in range(people + 1)]
+bottom = 2 ** (H)  # 말단 직원 숫자
+not_bottom = 2 ** (H) - 1  # 말단 직원이 아닌 직원의 숫자
 
-print(complete)
+for idx in range(bottom):  # 업무 할당
+    company[people - idx] = work[-1 - idx]
+
+# print(company)
+
+result = 0
+
+for r in range(R):
+    for i in range(1, not_bottom + 1):
+        if i == 1:
+            if len(company[1]) > 0:  # 부서장의 업무 중 처리된 업무가 있다면 그 번호를 더함
+                result = result + company[1].pop(0)
+
+        # print(r)
+        if r % 2 == 0:  # 날짜가 짝수일 때
+            if len(company[2 * i + 1]) > 0:
+                value = company[2 * i + 1].pop(0)
+                company[i].append(value)
+                # print(i,value)
+        elif r % 2 == 1:  # 날짜가 홀수일 때
+            if len(company[2 * i]) > 0:
+                value = company[2 * i].pop(0)
+                company[i].append(value)
+                # print(i,value)
+print(result)
